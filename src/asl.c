1ab7348d3da35d2bef292312d02887bc65052945
Mattia Guazzaloca <mattia.guazzaloca@gmail.com>
2020-02-06 11:53:19 +0100

    Added phase 1 intial stuff 2

---
+++ src/asl.c
@@ -0,0 +1,11 @@
+#include "asl.h"
+
+/* ASL handling functions */
+semd_t *getSemd(int *key) {}
+void initASL() {}
+
+int insertBlocked(int *key, pcb_t *p) {}
+pcb_t *removeBlocked(int *key) {}
+pcb_t *outBlocked(pcb_t *p) {}
+pcb_t *headBlocked(int *key) {}
+void outChildBlocked(pcb_t *p) {}
