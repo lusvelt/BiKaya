9fee55522abac580c30665d7fe21ee066292fc61
Damiano Scevola <damiano.scevola@studio.unibo.it>
2020-03-18 10:25:49 +0100

    Moved memset

--- include/memset.h
+++ include/memset.h
@@ -3,11 +3,6 @@
 
 typedef unsigned int size_t;
 
-void *memset(void *s, int c, size_t n) {
-    unsigned char *p = (unsigned char *)s;
-    while (n--)
-        *p++ = (unsigned char)c;
-    return s;
-}
+void *memset(void *s, int c, size_t n);
 
 #endif
---
+++ src/memset.c
@@ -0,0 +1,8 @@
+#include "memset.h"
+
+void *memset(void *s, int c, size_t n) {
+    unsigned char *p = (unsigned char *)s;
+    while (n--)
+        *p++ = (unsigned char)c;
+    return s;
+}
